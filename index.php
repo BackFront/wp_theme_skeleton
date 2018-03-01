43bd780e6451c33efacfb3b63e79792cae1c2b44
Douglas Alves <alves.douglaz@gmail.com>
2018-03-01 17:47:19 -0300

    teste Validator

--- index.php
+++ index.php
@@ -4,3 +4,10 @@
  */
 if(!defined('ABSPATH')) die('No direct script access allowed');
 echo $container['HelloWorld'];
+
+//Validator Teste
+echo "<hr> <h2>Validator deste</h2>";
+$valid = (new \App\Validators\TesteValidator())->with(['name' => '', 'email' => 'emialdot.net', 'password' => 'req23', 'confirm_password' => 'req123'])->passesOrFail();
+echo "<pre>";
+var_dump($valid);
+echo "</pre>";

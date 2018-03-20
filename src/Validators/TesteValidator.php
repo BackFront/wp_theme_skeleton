033f9b33a7fa8a6da1af644d6f2b4f08a3b8f214
Douglas Alves <alves.douglaz@gmail.com>
2018-03-20 13:50:48 -0300

    Pimple IOC service container

--- src/Validators/TesteValidator.php
+++ src/Validators/TesteValidator.php
@@ -3,6 +3,11 @@
 namespace App\Validators {
     class TesteValidator extends \Backfront\Validator\Validator
     {
-        public $rules = ['name' => 'required', 'email' => 'required|email', 'password' => 'required|min:6', 'confirm_password' => 'required|same:password'];
+        public $rules = [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+            'confirm_password' => 'required|same:password'
+        ];
     }
 }

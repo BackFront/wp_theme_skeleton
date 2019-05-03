65b0fea33d39a29d64439a7146c018f615090c2b
Douglas Alves <alves.douglaz@gmail.com>
2019-05-03 18:49:52 -0300

    criando testes

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
  * Silence is gold
  */
 if(!defined('ABSPATH')) die('No direct script access allowed');
-echo $container['HelloWorld'];
+//echo $container['HelloWorld'];
 
 //Validator Teste
-echo "<hr> <h2>Validator deste</h2>";
-$valid = (new \App\Validators\TesteValidator())->with(['name' => '', 'email' => 'emialdot.net', 'password' => 'req23', 'confirm_password' => 'req123'])->passesOrFail();
-echo "<pre>";
-var_dump($valid);
+//echo "<hr>Validator deste";
+//$valid = (new \App\Validators\TesteValidator())->with(['name' => '', 'email' => 'emialdot.net', 'password' => 'req23', 'confirm_password' => 'req123'])->passesOrFail();
+//echo "<pre>";
+//var_dump($valid);
 echo "</pre>";
